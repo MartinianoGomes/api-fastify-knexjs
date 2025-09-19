f77014c37bc9bcb92f4f8c3d5e37f9d4399190c4
Martíniano Gomes <uricanooriental@gmail.com>
2025-09-19 14:23:54 -0300

    Atualizando inserção de cursos.

---
+++ src/database/seeds/insert-courses.ts
@@ -0,0 +1,18 @@
+import { Knex } from "knex";
+
+export async function seed(knex: Knex): Promise<void> {
+    await knex("courses").insert([
+        { name: "CSS" },
+        { name: "Java" },
+        { name: "JavaScript" },
+        { name: "Git" },
+        { name: "Angular" },
+        { name: "React" },
+        { name: "PHP" },
+        { name: "Figma" },
+        { name: "C#" },
+        { name: "Ruby" },
+        { name: "Go" },
+        { name: "ChatGPT" },
+    ]);
+};
