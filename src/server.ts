3f48a6bb09bf0b1b7b3fa994ce35c7bb56f340a3
Martíniano Gomes <uricanooriental@gmail.com>
2025-09-23 14:54:52 -0300

    Atualizando server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,36 @@
 import fastify from "fastify";
+import { knex } from "./database/knex";
 
 const app = fastify()
 
-app.get('/',() => {
-  return "Hello Web II"
+interface Course {
+  id: number,
+  name: string
+}
+
+app.get('/courses', async (request, reply) => {
+  const courses = await knex("courses").select().orderBy("name")
+  return reply.status(201).send(courses)
 })
 
-app.get('/courses',() => {
-  return "Hello Curso"
+app.post('/courses', async (request, reply) => {
+  const { name } = request.body as Course
+  await knex("courses").insert({ name })
+  return reply.status(201).send({
+    message: "Curso adicionado com sucesso"
+  })
 })
 
-app.post('/courses',() => {
-  return "Cadastra Curso"
+app.put('/courses/:id', async (request, reply) => {
+  const { id } = request.params as Course
+  const { name } = request.body as Course
+  await knex("courses").update({ name }).where({ id })
+  return reply.status(201).send({
+    message: "Curso atualizado com sucesso"
+  })
 })
 
-app.listen({port:3333}).then(()=>{
+
+app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running")
 })
-
