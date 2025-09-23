c2db9170898947c67bdc7287919ff738d75fbf49
Martíniano Gomes <uricanooriental@gmail.com>
2025-09-23 14:45:57 -0300

    Atualizando migration

--- src/database/migrations/20250919171253_create-course-modules.ts
+++ src/database/migrations/20250919171253_create-course-modules.ts
@@ -4,11 +4,8 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.createTable("course_modules", (table) => {
         table.increments("id").primary();
-        table.integer("course_id").unsigned().notNullable()
-            .references("id").inTable("courses")
-            .onDelete("CASCADE");
-        table.text("title").notNullable();
-        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table.text("name").notNullable();
+        table.integer("course_id").notNullable().references("id").inTable("courses");
     });
 }
 
